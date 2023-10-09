03d32cb1435b4d1d083d53f46c4a87abafe222de
Murfidar <murfidar@gmail.com>
2023-10-09 15:54:22 +0700

    feat: setup development
    - setup basic server
    - create index.ts controller & routes
    - create notes.ts data

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
+import Fastify, { FastifyInstance } from 'fastify';
 
+const server: FastifyInstance = Fastify({
+  logger: true,
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: 3000, host: 'localhost' });
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
