df3cfa1711a4b6cf99e0a72a4e7ccf3e1f3b944d
Murfidar <murfidar@gmail.com>
2023-10-10 15:47:24 +0700

    feat: finished routes
    - fix bad routes
    - add schema for faster output

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,14 +4,98 @@ import {
   getAllNotes,
   getNoteById,
   updateNoteById,
-} from 'controller';
+} from '../controllers';
 import { FastifyInstance } from 'fastify';
+
+const messageSchema = {
+  type: 'object',
+  properties: {
+    status: { type: 'string' },
+    message: { type: 'string' },
+  },
+};
+
+const addNotesSchema = {
+  type: 'object',
+  properties: {
+    status: { type: 'string' },
+    message: { type: 'string' },
+    data: {
+      nodeId: { type: 'string' },
+    },
+  },
+};
+
+const getNotesOpts = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          status: { type: 'string' },
+          data: { type: 'array' },
+        },
+      },
+    },
+  },
+};
+
+const addNotesOpts = {
+  schema: {
+    response: {
+      200: addNotesSchema,
+      500: addNotesSchema,
+    },
+  },
+};
+
+const getNotesByIdOpts = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          status: { type: 'string' },
+          data: {
+            note: { type: 'object' },
+          },
+        },
+      },
+      404: messageSchema,
+    },
+  },
+};
+
+const editDeleteNotesOpts = {
+  schema: {
+    response: {
+      200: messageSchema,
+      404: messageSchema,
+    },
+  },
+};
+
+const helloOpts = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          ping: { type: 'string' },
+        },
+      },
+    },
+  },
+};
 
 export default async function routes(fastify: FastifyInstance) {
   fastify
-    .get('/notes', getAllNotes)
-    .post('/notes', addNotes)
-    .get('/notes', getNoteById)
-    .put('notes/:id', updateNoteById)
-    .delete('/notes/:id', deleteNoteById);
+    .get('/', helloOpts, async (req, reply) => {
+      reply.code(200).send({ ping: 'it worked' });
+    })
+    .get('/notes', getNotesOpts, getAllNotes)
+    .post('/notes', addNotesOpts, addNotes)
+    .get('/notes/:id', getNotesByIdOpts, getNoteById)
+    .put('/notes/:id', editDeleteNotesOpts, updateNoteById)
+    .delete('/notes/:id', editDeleteNotesOpts, deleteNoteById);
 }
