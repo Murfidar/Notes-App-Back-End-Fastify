375bd3c1185b1c901d496b7c972f1f1ab02e7148
Murfidar <murfidar@gmail.com>
2023-10-10 15:50:02 +0700

    feat: add swagger api

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,31 @@
 import Fastify, { FastifyInstance } from 'fastify';
-import routes from 'routes';
+import swagger from '@fastify/swagger';
+import swaggerUi from '@fastify/swagger-ui';
+import cors from '@fastify/cors';
+import routes from './routes';
 
 const server: FastifyInstance = Fastify({
   logger: true,
+});
+
+server.register(cors);
+server.register(swagger, {
+  swagger: {
+    info: {
+      title: 'Notes App API Docs',
+      description: 'This is an api docs created using swagger',
+      version: '1.0.0',
+    },
+    host: 'localhost:3000',
+    schemes: ['http', 'https'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    tags: [{ name: 'Default', description: 'Default' }],
+  },
+});
+
+server.register(swaggerUi, {
+  routePrefix: '/docs',
 });
 
 server.register(routes);
