d979cddb64b9b7dea71760128b0736a02220b09f
Murfidar <murfidar@gmail.com>
2023-10-10 15:49:42 +0700

    fix: change createdAt and updatedAt type

--- src/data/notes.ts
+++ src/data/notes.ts
@@ -3,8 +3,8 @@ export type note = {
   title: string;
   tags: string[];
   body: string;
-  createdAt: Date;
-  updatedAt: Date;
+  createdAt: string;
+  updatedAt: string;
 };
 
 export const notes: Array<note> = [];
